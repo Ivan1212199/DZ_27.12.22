a62ba2653ce5b47884c78808f1837c3cf279998d
Ivan <ivan18-18@example.com>
2022-12-25 21:54:37 +0300

    начинаем решать второю  задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,18 +1,16 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 Console.Write("Введите трёхзначное число: ");
-Num_1 = Console.WriteLine(Console.ReadLine());
+int Num_1 = int.Parse(Console.ReadLine()!);
 
 for (int ii = 1; ii < 3 && (Num_1 <= 100 | Num_1 > 999); ii++)
 {
     Console.Write(ii + " Кажется это не трёхзначное число, попробуем ещё раз: ");
-    Num_1 = Console.WriteLine(Console.ReadLine());
+    Num_1 = int.Parse(Console.ReadLine()!);
 }
 if (Num_1 <= 100 | Num_1 > 999)
 {
     Console.WriteLine("Чтото пошло не так, программа прервана: ");
-break;
 }
 
 Num_1 = Num_1 / 10 % 10;
 Console.WriteLine($"Вторая цифра введённого числа: {Num_1}");
-break;
