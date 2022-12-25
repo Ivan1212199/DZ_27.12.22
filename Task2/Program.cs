200ff460c7622e78a2cb112b043f69e873f52520
Ivan <ivan18-18@example.com>
2022-12-25 22:08:45 +0300

    закончили решать задачу, но что то пока не получается ее запустить

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите любое число: ");
+int num = int.Parse(Console.ReadLine());
+int numDigit = NumberDigit(num);
+
+if (numDigit <= 2)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    if (numDigit > 3)
+    {
+        num = num / int.Parse(Math.Pow(10, numDigit - 3));
+    }
+    num = num % 10;
+    Console.WriteLine($"Третья цифра введённого числа: {num}");
+}
